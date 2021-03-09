b4e75f040d64dfd4adb5c3e05a843032f9c3a345
Domantas Grubis <s037814@ad.viko.lt>
2021-03-09 18:09:47 +0200

    almost done

--- DES_Algorithm/Form1.cs
+++ DES_Algorithm/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,103 @@ namespace DES_Algorithm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = EncryptData(textBox1.Text, textBox3.Text);
+        }
+
+    public string Encrypt(string key, string text)
+        {
+            DES des= DES.Create();
+            MD5 md5 = MD5.Create();
+            byte[] byteBuff;
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            des.Key = null;
+            des.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Encoding.UTF8.GetBytes(text);
+
+            string encoded =
+                Convert.ToBase64String(des.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return encoded;
+        }
+        public string Decrypt(string text, string key)
+        {
+
+            DES des = DES.Create();
+            MD5 md5 = MD5.Create();
+
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            byte[] byteBuff;
+
+            des.Key = hash;
+            des.Mode = CipherMode.ECB; //CBC, CFB
+            byteBuff = Convert.FromBase64String(text);
+
+            string decoded =
+                Convert.ToBase64String(des.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length));
+            return decoded;
+  
+        }
+        public string EncryptData(string strData, string strKey)
+        {
+            byte[] key = { }; //Encryption Key   
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray;
+
+            try
+            {
+                key = Encoding.UTF8.GetBytes(strKey);
+                // DESCryptoServiceProvider is a cryptography class defind in c#.  
+                DESCryptoServiceProvider ObjDES = new DESCryptoServiceProvider();
+                inputByteArray = Encoding.UTF8.GetBytes(strData);
+                MemoryStream Objmst = new MemoryStream();
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateEncryptor(key, IV), CryptoStreamMode.Write);
+                Objcs.Write(inputByteArray, 0, inputByteArray.Length);
+                Objcs.FlushFinalBlock();
+
+                return Convert.ToBase64String(Objmst.ToArray());//encrypted string  
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public string DecryptData(string strData, string strKey)
+        {
+            byte[] key = { };// Key   
+            byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] inputByteArray = new byte[strData.Length];
+
+            try
+            {
+                key = Encoding.UTF8.GetBytes(strKey);
+                DESCryptoServiceProvider ObjDES = new DESCryptoServiceProvider();
+                inputByteArray = Convert.FromBase64String(strData);
+
+                MemoryStream Objmst = new MemoryStream();
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateDecryptor(key, IV), CryptoStreamMode.Write);
+                Objcs.Write(inputByteArray, 0, inputByteArray.Length);
+                Objcs.FlushFinalBlock();
+
+                Encoding encoding = Encoding.UTF8;
+                return encoding.GetString(Objmst.ToArray());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = DecryptData(textBox2.Text, textBox3.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
