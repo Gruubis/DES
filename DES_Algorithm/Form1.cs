ebf663dae932635b3f8f80318a9607068efbe2d7
Domantas Grubis <s037814@ad.viko.lt>
2021-03-10 11:17:37 +0200

    almost done1

--- DES_Algorithm/Form1.cs
+++ DES_Algorithm/Form1.cs
@@ -21,16 +21,27 @@ namespace DES_Algorithm
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(radioButton1.Checked == true)
-            textBox5.Text = EncryptData(textBox1.Text, textBox3.Text, true);
+            if (radioButton1.Checked == true)
+            {
+                string encryptedText = EncryptData(textBox1.Text, textBox3.Text, true);
+                if (checkBox1.Checked)
+                    File.WriteAllText(@"C:\Users\rolik\OneDrive\Desktop\New folder\EncryptedText.txt", encryptedText);
+                textBox5.Text = encryptedText;
+                
+            }
             else
-                textBox5.Text = EncryptData(textBox1.Text, textBox3.Text, false);
+            {
+               string  encryptedText = EncryptData(textBox1.Text, textBox3.Text, false);
+                if (checkBox1.Checked)
+                    File.WriteAllText(@"C:\Users\rolik\OneDrive\Desktop\New folder\EncryptedText.txt", encryptedText);
+                    textBox5.Text = encryptedText;
+            }
         }
 
         public string EncryptData(string strData, string strKey, bool EBC)
         {
-            byte[] key = new byte[8];
             byte[] IV = { 10, 20, 30, 40, 50, 60, 70, 80 };
+            byte[] key = new byte[8];
             byte[] inputByteArray;
             byte[] hash;
 
@@ -46,14 +57,17 @@ namespace DES_Algorithm
                 if (EBC == true)
                 {
                     ObjDES.Mode = CipherMode.ECB;
+                    ObjDES.Key = key;
                 }
                 else
                 {
                     ObjDES.Mode = CipherMode.CBC;
+                    ObjDES.Key = key;
+                    ObjDES.IV = IV;
                 }
                     inputByteArray = Encoding.ASCII.GetBytes(strData);
                 MemoryStream Objmst = new MemoryStream();
-                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateEncryptor(key, IV), CryptoStreamMode.Write);
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateEncryptor(), CryptoStreamMode.Write);
                 Objcs.Write(inputByteArray, 0, inputByteArray.Length);
                 Objcs.FlushFinalBlock();
 
@@ -82,15 +96,18 @@ namespace DES_Algorithm
                 if (EBC == true)
                 {
                     ObjDES.Mode = CipherMode.ECB;
+                    ObjDES.Key = key;
                 }
                 else
                 {
                     ObjDES.Mode = CipherMode.CBC;
+                    ObjDES.Key = key;
+                    ObjDES.IV = IV;
                 }
                 inputByteArray = Convert.FromBase64String(strData);
 
                 MemoryStream Objmst = new MemoryStream();
-                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateDecryptor(key, IV), CryptoStreamMode.Write);
+                CryptoStream Objcs = new CryptoStream(Objmst, ObjDES.CreateDecryptor(), CryptoStreamMode.Write);
                 Objcs.Write(inputByteArray, 0, inputByteArray.Length);
                 Objcs.FlushFinalBlock();
 
@@ -106,9 +123,25 @@ namespace DES_Algorithm
         private void button2_Click(object sender, EventArgs e)
         {
             if (radioButton1.Checked == true)
-                textBox6.Text = DecryptData(textBox2.Text, textBox4.Text, true);
+            {
+                if (checkBox2.Checked)
+                {
+                    textBox6.Text = DecryptData(File.ReadAllText(@"C:\Users\rolik\OneDrive\Desktop\New folder\EncryptedText.txt"), textBox4.Text, true);
+                }
+                else
+                {
+                    textBox6.Text = DecryptData(textBox2.Text, textBox4.Text, true);
+                }
+            }
             else
-                textBox6.Text = DecryptData(textBox2.Text, textBox4.Text, false);
+            {
+                if (checkBox2.Checked)
+                {
+                    textBox6.Text = DecryptData(File.ReadAllText(@"C:\Users\rolik\OneDrive\Desktop\New folder\EncryptedText.txt"), textBox4.Text, false);
+                }
+                else
+                    textBox6.Text = DecryptData(textBox2.Text, textBox4.Text, false);
+            }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
